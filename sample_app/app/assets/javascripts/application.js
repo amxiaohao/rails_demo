85d8c06b6abeaca45731acf7015403b8f2c8e63c
dizzy <amxiaohao@gmail.com>
2017-07-27 23:37:04 +0800

    finish basic login

--- sample_app/app/assets/javascripts/application.js
+++ sample_app/app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 
-//= require bootstrap
 //= require jquery
-//= require jquery-ujs
+//= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
